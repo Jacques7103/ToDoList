c8eef8cfce949af5786c0740eaf88bdeedfcd762
Jacques7103 <ferdinand.jacques@binus.ac.id>
2023-03-10 22:24:15 +0700

    Added self data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-// import Title from './components/Title'
-// import List from './components/List';
 import Front from './components/Front';
 
 function App() {
   return (
     <div className="App">
       <h1 className='title'>To Do List</h1>
+      <div className='data'>
+        <p>Name : Ferdinand Jacques</p>
+        <p>NIM : 2501982600</p>
+        <p>Class : L4AC</p>
+      </div>
       <Front />
     </div>
   );
